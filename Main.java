3cc8d207761225e30f52ac914568f909cd9fd2de
Leonardo Spilere <leonardo.spilere@deps.com.br>
2024-09-16 22:58:09 -0300

    add start of graphical interface

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-    	String directoryPath = "C:\\Users\\Natanael\\Documents\\dev\\parallel-computing\\datasets\\dataset_g";
-        String nameToFind = "Edward Martin";
-        
-        FindersCompare.execute(directoryPath, nameToFind);
+        JFrame frame = new JFrame("Main");
+        frame.setContentPane(new Window().getMainPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
